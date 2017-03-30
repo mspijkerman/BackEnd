4af70bcd8dccc6c9c8bb2cf319b714338726607a
mspijkerman <mspijkerman02@student.rocvantwente.nl>
2017-03-30 15:02:40 +0200

    Opdracht 6
    Opdracht 6 toegevoeg

---
+++ Opdracht6/Default.aspx
@@ -0,0 +1,106 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .button{
+            width: 60px;
+            height: 60px;
+            font-size: 30px;
+        }
+        #btnBack{
+            font-size: 25px;
+        }
+        #btn0{
+            width: 120px
+        }
+        #txtScherm{
+            font-size: 50px;
+            text-align: right;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        
+        <br />
+        <table class="auto-style1"><asp:TextBox ID="txtScherm" runat="server" Height="57px" Width="309px" OnTextChanged="TextBox1_TextChanged" Text="0" ReadOnly="True">0</asp:TextBox>
+            <tr>
+                <td>
+                    <asp:Button CssClass="button" ID="btn7" runat="server" OnClick="Button7_Click" Text="7" />
+                </td>
+                <td>
+                    <asp:Button CssClass="button" ID="btn8" runat="server" OnClick="Button8_Click" Text="8" />
+                </td>
+                <td>
+                    <asp:Button CssClass="button" ID="btn9" runat="server" OnClick="Button9_Click" Text="9" />
+                </td>
+                <td>
+                    <asp:Button CssClass="button" ID="btnC" runat="server" OnClick="ButtonC_Click" Text="C" />
+                </td>
+                <td>
+                    <asp:Button CssClass="button" ID="btnBack" runat="server" OnClick="ButtonBack_Click" Text="Back" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button CssClass="button" ID="btn4" runat="server" OnClick="Button4_Click" Text="4" />
+                </td>
+                <td>
+                    <asp:Button CssClass="button" ID="btn5" runat="server" OnClick="Button5_Click" Text="5" />
+                </td>
+                <td>
+                    <asp:Button CssClass="button" ID="btn6" runat="server" OnClick="Button6_Click" Text="6" />
+                </td>
+                <td>
+                    <asp:Button CssClass="button" ID="btnPlus" runat="server" OnClick="ButtonPlus_Click" Text="+" />
+                </td>
+                <td>
+                    <asp:Button CssClass="button" ID="btnM2" runat="server" OnClick="ButtonM2_Click" Text="M2" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button CssClass="button" ID="btn1" runat="server" OnClick="Button1_Click" Text="1" />
+                </td>
+                <td>
+                    <asp:Button CssClass="button" ID="btn2" runat="server" OnClick="Button2_Click" Text="2" />
+                </td>
+                <td>
+                    <asp:Button CssClass="button" ID="btn3" runat="server" OnClick="Button3_Click" Text="3" />
+                </td>
+                <td>
+                    <asp:Button CssClass="button" ID="btnMin" runat="server" OnClick="ButtonMin_Click" Text="-" />
+                </td>
+                <td>
+                    <asp:Button CssClass="button" ID="btnO1" runat="server" OnClick="ButtonO1_Click" Text="O1" />
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Button CssClass="button" ID="btn0" runat="server" OnClick="Button0_Click" Text="0" />
+                </td>
+                <td>
+                    <asp:Button CssClass="button" ID="btnPunt" runat="server" OnClick="ButtonPunt_Click" Text="." />
+                </td>
+                <td>
+                    <asp:Button CssClass="button" ID="btnIs" runat="server" OnClick="ButtonIs_Click" Text="=" />
+                </td>
+                <td>
+                    <asp:Button CssClass="button" ID="btnO2" runat="server" OnClick="ButtonO2_Click" Text="O2" />
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+        <p>
+            <asp:Label ID="lblGetal1" runat="server" Visible="false" Text=""></asp:Label>
+            <asp:Label ID="lblBereken" runat="server" Visible="false" Text=""></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Opdracht6/Default.aspx.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void ButtonPunt_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Contains(",") != true)
+        {
+            txtScherm.Text += ",";
+        }
+    }
+
+    protected void Button0_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            if(txtScherm.Text != "0")
+            {
+                txtScherm.Text += "0";
+            }
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            if(txtScherm.Text == "0")
+            {
+                txtScherm.Text = "1";
+            }
+            else
+            {
+                txtScherm.Text += "1";
+            }
+        }
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            if (txtScherm.Text == "0")
+            {
+                txtScherm.Text = "2";
+            }
+            else
+            {
+                txtScherm.Text += "2";
+            }
+        }
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            if (txtScherm.Text == "0")
+            {
+                txtScherm.Text = "3";
+            }
+            else
+            {
+                txtScherm.Text += "3";
+            }
+        }
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            if (txtScherm.Text == "0")
+            {
+                txtScherm.Text = "4";
+            }
+            else
+            {
+                txtScherm.Text += "4";
+            }
+        }
+    }
+
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            if (txtScherm.Text == "0")
+            {
+                txtScherm.Text = "5";
+            }
+            else
+            {
+                txtScherm.Text += "5";
+            }
+        }
+    }
+
+    protected void Button6_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            if (txtScherm.Text == "0")
+            {
+                txtScherm.Text = "6";
+            }
+            else
+            {
+                txtScherm.Text += "6";
+            }
+        }
+    }
+
+    protected void Button7_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            if (txtScherm.Text == "0")
+            {
+                txtScherm.Text = "7";
+            }
+            else
+            {
+                txtScherm.Text += "7";
+            }
+        }
+    }
+
+    protected void Button8_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            if (txtScherm.Text == "0")
+            {
+                txtScherm.Text = "8";
+            }
+            else
+            {
+                txtScherm.Text += "8";
+            }
+        }
+    }
+
+    protected void Button9_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            if (txtScherm.Text == "0")
+            {
+                txtScherm.Text = "9";
+            }
+            else
+            {
+                txtScherm.Text += "9";
+            }
+        }
+    }
+
+    protected void ButtonC_Click(object sender, EventArgs e)
+    {
+        txtScherm.Text = "0";
+        lblGetal1.Text = "";
+    }
+
+    protected void ButtonPlus_Click(object sender, EventArgs e)
+    {
+        lblGetal1.Text = txtScherm.Text;
+        txtScherm.Text = "0";
+        lblBereken.Text = "+";
+    }
+
+    protected void ButtonMin_Click(object sender, EventArgs e)
+    {
+        lblGetal1.Text = txtScherm.Text;
+        txtScherm.Text = "0";
+        lblBereken.Text = "-";
+    }
+
+    protected void ButtonIs_Click(object sender, EventArgs e)
+    {
+        double G1 = Convert.ToDouble(txtScherm.Text);
+        double G2 = Convert.ToDouble(lblGetal1.Text);
+        double Uitkomst = 0;
+        if (lblBereken.Text == "+")
+        {
+            Uitkomst = G1 + G2;
+        }
+        if (lblBereken.Text == "-")
+        {
+            Uitkomst = G2 - G1;
+        }
+        txtScherm.Text = Uitkomst.ToString();
+    }
+
+    protected void ButtonBack_Click(object sender, EventArgs e)
+    {
+    if (txtScherm.Text != "0")
+        {
+            txtScherm.Text = txtScherm.Text.Remove(txtScherm.Text.Length - 1, 1);
+            if (txtScherm.Text == "")
+            {
+                txtScherm.Text = "0";
+            }
+        }
+    }
+
+    protected void ButtonM2_Click(object sender, EventArgs e)
+    {
+    
+    }
+
+    protected void ButtonO1_Click(object sender, EventArgs e)
+    {
+        double G1 = Convert.ToDouble(txtScherm.Text);
+        double Uitkomst = (Math.PI * (G1 * G1));
+        txtScherm.Text = Uitkomst.ToString();
+    }
+
+    protected void ButtonO2_Click(object sender, EventArgs e)
+    {
+        double G1 = Convert.ToDouble(txtScherm.Text);
+        double Uitkomst = (Math.PI * G1);
+        txtScherm.Text = Uitkomst.ToString();
+    }
+}
